2edf8c2e8625d1393263101b84615d246d521f1f
ramzzzn <bomike3@gmail.com>
2024-05-06 23:22:10 +0300

    This is homework №8: add remote + local execute

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import logging
 import datetime
 import os
+import random
 
 import pytest
 from selenium import webdriver
@@ -14,11 +15,16 @@ from selenium.webdriver.safari.service import Service as SafariService
 
 
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="Chrome")
+    parser.addoption("--browser", action="store", default="chrome")
+    parser.addoption("--remote", action="store_true", default=True)
+    parser.addoption("--executor", action="store", default="127.0.0.1")
     parser.addoption("--base_url", action="store", default="http://172.22.144.70:8081")
     parser.addoption("--headless", action="store_true", default=False)
     parser.addoption("--ya_driver", default="C:/otus_homeworks/homework_web/drivers/yandexdriver.exe")
     parser.addoption("--log_level", action="store", default="INFO")
+    parser.addoption("--brws_vers", default="124.0")
+    parser.addoption("--video", action="store_true", default=False)
+    parser.addoption("--vnc", action="store_true", default=True)
 
 
 def setup_logger(name: str, log_level: str) -> logging.Logger:
@@ -35,6 +41,75 @@ def setup_logger(name: str, log_level: str) -> logging.Logger:
     return logger
 
 
+def local_execute(browser_name, headless, ya_driver):
+    # настраиваем веб-драйвер, согласно выбранному браузеру:
+    if browser_name == "chrome":
+        service = ChromiumService()
+        options = ChromiumOptions()
+        if headless:
+            options.add_argument("headless=new")
+        driver = webdriver.Chrome(service=service, options=options)
+    elif browser_name == "firefox":
+        service = FirefoxService()
+        options = FirefoxOptions()
+        if headless:
+            options.add_argument("-headless")
+        driver = webdriver.Firefox(service=service, options=options)
+    elif browser_name == "edge":
+        service = EdgeService()
+        options = EdgeOptions()
+        if headless:
+            options.add_argument("headless=new")
+        driver = webdriver.Edge(service=service, options=options)
+    elif browser_name == "yandex":
+        service = ChromiumService(executable_path=ya_driver)
+        options = ChromiumOptions()
+        if headless:
+            options.add_argument("headless=new")
+        driver = webdriver.Chrome(service=service, options=options)
+    elif browser_name == "safari":
+        service = SafariService()
+        driver = webdriver.Safari(service=service)
+
+    return driver
+
+
+def remote_execute(executor, browser_name, version, vnc, video):
+    executor_url = f"http://{executor}:4444/wd/hub"
+    # настраиваем веб-драйвер, согласно выбранному браузеру:
+    if browser_name == "chrome":
+        options = ChromiumOptions()
+    elif browser_name == "firefox":
+        options = FirefoxOptions()
+    elif browser_name == "edge":
+        options = EdgeOptions()
+
+    caps = {
+        "browserName": browser_name,
+        "browserVersion": version,
+        "selenoid:options": {
+            "enableVNC": vnc,
+            "name": os.getenv("Homework_№8", str(random.randint(9000, 10000))),
+            "enableVideo": video,
+            "timeZone": "Europe/Moscow",
+            "env": ["LANG=ru_RU.UTF-8", "LANGUAGE=ru:en", "LC_ALL=ru_RU.UTF-8"],
+            "sessionTimeout": "1m"
+
+        },
+        "acceptInsecureCerts": True,
+    }
+
+    for k, v in caps.items():
+        options.set_capability(k, v)
+
+    driver = webdriver.Remote(
+        command_executor=executor_url,
+        options=options
+    )
+
+    return driver
+
+
 @pytest.fixture
 def browser(request):
     # присваиваем полученные значения при запуске
@@ -43,39 +118,22 @@ def browser(request):
     headless = request.config.getoption("--headless")
     ya_driver = request.config.getoption("--ya_driver")
     log_level = request.config.getoption("--log_level")
+    remote = request.config.getoption("--remote")
+    executor = request.config.getoption("--executor")
+    version = request.config.getoption("--brws_vers")
+    video = request.config.getoption("--video")
+    vnc = request.config.getoption("--video")
 
     # настраиваем логирование
     logger = setup_logger(request.node.name, log_level)
     logger.info("===> Test %s started at %s" % (request.node.name, datetime.datetime.now()))
 
-    # настраиваем веб-драйвер, согласно выбранному браузеру:
-    if browser_name == "Chrome":
-        service = ChromiumService()
-        options = ChromiumOptions()
-        if headless:
-            options.add_argument("headless=new")
-        _browser = webdriver.Chrome(service=service, options=options)
-    elif browser_name == "Firefox":
-        service = FirefoxService()
-        options = FirefoxOptions()
-        if headless:
-            options.add_argument("-headless")
-        _browser = webdriver.Firefox(service=service, options=options)
-    elif browser_name == "Edge":
-        service = EdgeService()
-        options = EdgeOptions()
-        if headless:
-            options.add_argument("headless=new")
-        _browser = webdriver.Edge(service=service, options=options)
-    elif browser_name == "Yandex":
-        service = ChromiumService(executable_path=ya_driver)
-        options = ChromiumOptions()
-        if headless:
-            options.add_argument("headless=new")
-        _browser = webdriver.Chrome(service=service, options=options)
-    elif browser_name == "Safari":
-        service = SafariService()
-        _browser = webdriver.Safari(service=service)
+    # если запуск тестов выполняется удаленно
+    if remote:
+        _browser = remote_execute(executor, browser_name, version, vnc, video)
+    # если запуск тестов выполняется локально
+    else:
+        _browser = local_execute(browser_name, headless, ya_driver)
 
     _browser.maximize_window()
     _browser.log_level = log_level
